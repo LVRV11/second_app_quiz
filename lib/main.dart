4c83e4301f60b7fab82f4c3145ada0396681ced5
LVRV11 <lavrovdmitrii@gmail.com>
2023-10-10 23:57:21 +0200

    dd

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,19 @@ import 'package:second_app_quiz/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromRGBO(137, 83, 198, 1),
+              ],
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
