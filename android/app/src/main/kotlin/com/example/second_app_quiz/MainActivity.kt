0e2f9416307cdfa4431585de5ff02d3cc183f380
LVRV11 <lavrovdmitrii@gmail.com>
2023-10-10 23:45:02 +0200

    first step quiz

---
+++ android/app/src/main/kotlin/com/example/second_app_quiz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.second_app_quiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:second_app_quiz/start_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
+
+
+// Image(image: 'assets/images/quiz-logo.pmg')
