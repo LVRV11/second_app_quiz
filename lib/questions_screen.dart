9825fa7c3ef4c0f74bb20921599691375f1a582e
LVRV11 <lavrovdmitrii@gmail.com>
2023-10-16 23:53:04 +0200

    3в

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -44,9 +44,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             const SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map((answer) {
               return AnswerButton(
-                answerText: answer,
-                onTap: answerQuestion,
-              );
+                  answerText: answer, onTap: answerQuestion, 12);
             })
           ],
         ),
